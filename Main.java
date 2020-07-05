02ee2fc4b9bc237e0e616651d5a13b72ffe46fb6
isaacItz <isaac.est.cbtis@gmail.com>
2020-07-04 23:52:34 -0500

    añade el valor de pi

--- Main.java
+++ Main.java
@@ -8,6 +8,7 @@ class Main{
   public static int hacerSuma(int n1, int n2){
     int variable = 123;
     System.out.println("la suma sera retornada");
+    Double PI = 3.141516;
     return n1 * n2;
     //hasta la bista beibi
     //jaaja

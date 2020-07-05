db7d5e045ba937950cc7d5a1fb356aea16e8a6ef
isaacItz <isaac.est.cbtis@gmail.com>
2020-07-04 23:43:33 -0500

    devuelve un agradable mensaje

--- Main.java
+++ Main.java
@@ -6,6 +6,7 @@ class Main{
 
   //hace una suma de dos numeros enteros
   public static int hacerSuma(int n1, int n2){
+    System.out.println("el resultado de la suma es devuelto");
     return n1 + n2;
   }
 

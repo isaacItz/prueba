4c29c8a094feeb40f134ed9d0e48a44e3bb5aeb8
isaacItz <isaac.est.cbtis@gmail.com>
2020-07-04 23:50:57 -0500

    añade espacio en blanco inutil

--- Main.java
+++ Main.java
@@ -7,6 +7,7 @@ class Main{
   //hace una suma de dos numeros enteros
   public static int hacerSuma(int n1, int n2){
     System.out.println("la suma sera retornada");
+
     return n1 * n2;
     //hasta la bista beibi
     //jaaja
